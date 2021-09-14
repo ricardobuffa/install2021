696b9035cd850532bececff1f65ce2c3e5a6ca0b
Ricardo Buffa Verçosa <ricardobuffa@gmail.com>
2021-09-13 21:44:48 -0300

    Update apt based so
    Update apt based so

---
+++ update_so.sh
@@ -0,0 +1,15 @@
+apt clean
+apt update
+apt update -m
+dpkg --configure -a
+apt upgrade
+apt install --assume-yes -y -f
+apt dist-upgrade --assume-yes -y
+apt autoremove --assume-yes -y --purge
+apt autoclean --assume-yes -y
+apt install --assume-yes -y -f
+apt update
+apt upgrade
+fwupdmgr get-devices
+fwupdmgr get-updates
+fwupdmgr update
